524efb92cd87bad20d4d33a30313b1279186b3eb
Isaac-Ronald-Ward <isaacronaldward@gmail.com>
2018-01-16 14:30:50 +0800

    Increased batch size to 16 to experiment with effects of post ReLU batch normalisation

--- src/main.cpp
+++ src/main.cpp
@@ -43,7 +43,7 @@ const int INPUT_WIDTH = 32;
 const int INPUT_HEIGHT = 32;
 
 //Number of images to feed per batch (minibatch)
-const int BATCH_SIZE = 8;
+const int BATCH_SIZE = 16;
 
 //Whether or not to reun evaluation of CPU or NCS'
 const bool EVALUATE_ON_NCS = false;
